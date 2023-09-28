09ef055a8b1450e54df147a7b1e41441004a2bbd
Abu Arefin Ali <arefinn@users.noreply.github.com>
2023-09-28 21:30:35 +0530

    Added questions as comments  (#4)
    * Case Study 2 commit
    
    Case Study 2 commit
    
    * Added questions as comments
    
    Added questions as comments

--- case-study-2/1.py
+++ case-study-2/1.py
@@ -1,3 +1,13 @@
+'''
+1. Create 1st tuple with values -> (10, 20, 30), 2nd tuple with values -> (40,
+50, 60):
+a. Concatenate the two tuples and store it in “t_combine”
+b. Repeat the elements of “t_combine” 3 times
+c. Access the 3rd element from “t_combine”
+d. Access the first three elements from “t_combine”
+e. Access the last three elements from “t_combine
+'''
+
 tuple1 = tuple()
 tuple2 = tuple()
 
--- case-study-2/2.py
+++ case-study-2/2.py
@@ -1,3 +1,10 @@
+'''
+2. Create a list ‘my_list’ with these elements:
+a. First element is a tuple with values 1, 2, 3
+b. Second element is a tuple with values “a”, “b”, “c”
+c. Third element is a tuple with values True, False
+'''
+
 my_list = list()
 my_list = [(1, 2, 3), ('a', 'b', 'c'), (True, False)]
 print(my_list)
