64372b38484a136f7547a2e932c04f7ea40fc107
Abu Arefin Ali <arefinn@gmail.com>
2023-09-28 21:23:48 +0530

    Added questions as comments
    Added questions as comments

--- case-study-2/3.py
+++ case-study-2/3.py
@@ -1,3 +1,8 @@
+'''
+3. Append a new tuple – (1, ‘a’, True) to ‘my_list’:
+a. Append a new - *"Sparta", 23+ to my_list
+'''
+
 my_list = list()
 my_list = [(1, 2, 3), ('a', 'b', 'c'), (True, False)]
 my_list.append((1, 'a', True))
--- case-study-2/5.py
+++ case-study-2/5.py
@@ -1,3 +1,7 @@
+'''
+5. Create a set named ‘my_set’ with values (1, 1, “a”, “a”, True, True) and print the result.
+'''
+
 my_set = set()
 my_set = {1, 1, "a", "a", True, True}
 print(my_set)
